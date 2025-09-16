f7357f52b29fd38e8703597b22e7caf82a964536
Codidharma - Mandar Dharmadhikari <codidharma@gmail.com>
2025-09-16 19:12:44 +0530

    chore: add birth_date and related logic in the user class

--- src/personal_finance_app/api/modules/ledger/householdaggregate/user.py
+++ src/personal_finance_app/api/modules/ledger/householdaggregate/user.py
@@ -1,5 +1,6 @@
 import re
 from dataclasses import dataclass
+from datetime import datetime
 from personal_finance_app.api.sharedkernel.domain.base_entity import BaseEntity
 
 
@@ -7,8 +8,9 @@ from personal_finance_app.api.sharedkernel.domain.base_entity import BaseEntity
 class User(BaseEntity):
     name: str
     email: str
+    birth_date: datetime
 
-    def __init__(self, name: str, email: str):
+    def __init__(self, name: str, email: str, birth_date: datetime):
 
         if not name or name.isspace():
             raise ValueError("name cannot be empty or whitespace.")
@@ -18,6 +20,36 @@ class User(BaseEntity):
         if re.fullmatch(email_regex, email) is None:
             raise ValueError("email should be of format abc@example.com.")
 
+        _validate_age(birth_date)
+
         super().__init__()
         self.name = name
         self.email = email
+        self.birth_date = birth_date
+
+
+def _calculate_age(birth_date: datetime) -> int:
+
+    reference_date = datetime.today()
+
+    age = reference_date.year - birth_date.year
+
+    # Adjust if birthday hasn't occurred this year yet
+    if (reference_date.month, reference_date.day) < (birth_date.month, birth_date.day):
+        age -= 1
+
+    return age
+
+
+def _validate_age(birth_date: datetime):
+
+    if birth_date > datetime.now():
+        raise ValueError("birth date cannot be in future.")
+
+    if birth_date == datetime.now():
+        raise ValueError("birth date can not be today.")
+
+    AGE_THRESHOLD: int = 16
+
+    if _calculate_age(birth_date) < AGE_THRESHOLD:
+        raise ValueError("user should be at least sixteen years old as on today.")
--- tests/api/modules/ledger/householdaggregate/test_user.py
+++ tests/api/modules/ledger/householdaggregate/test_user.py
@@ -1,36 +1,75 @@
 import pytest
 from personal_finance_app.api.modules.ledger.householdaggregate.user import User
+from datetime import datetime, timedelta
 
 
 def test_new_should_create_user_instance():
+
     name = "Mandar Dharmadhikari"
     email = "test@value.com"
-    user = User(name, email)
+    birth_date = datetime(1989, 1, 1)
+
+    user = User(name, email, birth_date)
 
     assert user.name == name
     assert user.email == email
+    assert user.birth_date == birth_date
     assert user.id is not None
 
 
 def test_invalid_name_should_raise_value_error():
 
     email = "mandar@test.com"
+    birth_date = datetime(1989, 1, 1)
 
     with pytest.raises(ValueError, match="name cannot be empty or whitespace."):
-        User("", email)
+        User("", email, birth_date)
     with pytest.raises(ValueError, match="name cannot be empty or whitespace."):
-        User(" ", email)
+        User(" ", email, birth_date)
 
 
 def test_invalid_email_should_raise_value_error():
 
     name = "Mandar Dharmadhikari"
+    birth_date = datetime(1989, 1, 1)
 
     with pytest.raises(ValueError, match="email should be of format abc@example.com."):
-        User(name, "")
+        User(name, "", birth_date)
 
     with pytest.raises(ValueError, match="email should be of format abc@example.com."):
-        User(name, " ")
+        User(name, " ", birth_date)
 
     with pytest.raises(ValueError, match="email should be of format abc@example.com."):
-        User(name, "abc")
+        User(name, "abc", birth_date)
+
+
+def test_user_should_not_be_born_in_future():
+
+    name = "Mandar Dharmadhikari"
+    email = "mandar@test.com"
+    birth_date = datetime(2100, 1, 1)
+
+    with pytest.raises(ValueError, match="birth date cannot be in future."):
+        User(name, email, birth_date)
+
+
+def test_user_should_not_be_born_today():
+
+    name = "Mandar Dharmadhikari"
+    email = "mandar@test.com"
+    birth_date = datetime.now()
+
+    with pytest.raises(ValueError, match="birth date can not be today."):
+        User(name, email, birth_date)
+
+
+def test_user_should_be_atleast_sixteen_years_old():
+
+    name = "Mandar Dharmadhikari"
+    email = "mandar@test.com"
+    birth_date = datetime.now() - timedelta(days=365)
+
+    with pytest.raises(
+        ValueError, match="user should be at least sixteen years old as on today."
+    ):
+        User(name, email, birth_date)
