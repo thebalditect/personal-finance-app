7dcc75a7df51d5790fdb6685dcf0f8a1ced831d8
Codidharma - Mandar Dharmadhikari <codidharma@gmail.com>
2025-09-20 00:34:23 +0530

    refactor: add tests for guard clauses in result class

--- tests/personal_finance_app/api/sharedkernel/domain/test_result.py
+++ tests/personal_finance_app/api/sharedkernel/domain/test_result.py
@@ -1,3 +1,4 @@
+import pytest
 from personal_finance_app.api.sharedkernel.domain.error import Error
 from personal_finance_app.api.sharedkernel.domain.error_type import ErrorType
 from personal_finance_app.api.sharedkernel.domain.result import Result
@@ -58,3 +59,26 @@ def test_failure_result_with_multiple_errors():
     assert len(result.errors) == 2
     assert result.errors[0] == error1
     assert result.errors[1] == error2
+
+
+def test_accessing_value_for_failure_result_should_raise_attribute_error():
+
+    error = Error(
+        "Error.Validation", "Validation for field a failed", ErrorType.VALIDATION
+    )
+    result = Result.failure(error)
+
+    with pytest.raises(
+        AttributeError, match="Cannot access `value` on a failed result."
+    ):
+        result.value
+
+
+def test_accessing_errors_for_success_result_should_raise_attribute_error():
+
+    result = Result.success(None)
+
+    with pytest.raises(
+        AttributeError, match="Cannot access `errors` on a success result."
+    ):
+        result.errors
